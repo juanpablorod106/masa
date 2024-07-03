fedde0bd91daf6705c8f8351733911b4a4eef3ef
JUANCHOROIS <123810423+juanpablorod106@users.noreply.github.com>
2024-07-03 17:24:01 -0400

    POO - Clase 9 - Visual Studio - Actividad Ticket de Cine
    4ta practica - Ejercicio del ticket de Cine.

---
+++ Ticket de Cine/Class1.vb
@@ -0,0 +1,84 @@
+﻿Public Class cine
+    Private _nombre As String
+    Private _id As Integer
+    Private _sala As String
+    Private _asiento As String
+    Private _pelicula As String
+    Private _hora As String
+    Private _funcion As String
+    '"Public" Para acceder a estas variables en cualquier clase.'
+
+    Public Sub New(ByVal nombre As String, ByVal id As Integer, ByVal sala As String, ByVal asiento As String, ByVal pelicula As String, ByVal hora As String, ByVal funcion As String)
+        Me._nombre = nombre
+        Me._id = id
+        Me._sala = sala
+        Me._asiento = asiento
+        Me._pelicula = pelicula
+        Me._hora = hora
+        Me._funcion = funcion
+        'Ejemplo de Constructor en VB'
+    End Sub
+    Public Property nombre As String 'Property, Getter and Setter'
+        Get
+            Return _nombre
+        End Get
+        Set(value As String)
+            Me._nombre = value
+        End Set
+    End Property
+
+    Public Property id As Integer
+        Get
+            Return _id
+        End Get
+        Set(value As Integer)
+            Me._id = value
+        End Set
+    End Property
+
+    Public Property sala As String
+        Get
+            Return _sala
+        End Get
+        Set(value As String)
+            Me._sala = value
+        End Set
+    End Property
+
+    Public Property asiento As String
+        Get
+            Return _asiento
+        End Get
+        Set(value As String)
+            Me._asiento = value
+        End Set
+    End Property
+
+    Public Property pelicula As String
+        Get
+            Return _pelicula
+        End Get
+        Set(value As String)
+            Me._pelicula = value
+        End Set
+    End Property
+
+    Public Property hora As String
+        Get
+            Return _hora
+        End Get
+        Set(value As String)
+            Me._hora = value
+        End Set
+    End Property
+
+    Public Property funcion As String
+        Get
+            Return _funcion
+        End Get
+        Set(value As String)
+            Me._funcion = value
+        End Set
+    End Property
+    'Se usa encapsulacion para evitar la manipulación de código y  alteración de datos por quien no está autorizado.'
+End Class
---
+++ Ticket de Cine/Form1.Designer.vb
@@ -0,0 +1,350 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lbl_name = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.tbx_name = New System.Windows.Forms.TextBox()
+        Me.tbx_id = New System.Windows.Forms.TextBox()
+        Me.tbx_stage = New System.Windows.Forms.TextBox()
+        Me.tbx_site = New System.Windows.Forms.TextBox()
+        Me.tbx_movie = New System.Windows.Forms.TextBox()
+        Me.tbx_hour = New System.Windows.Forms.TextBox()
+        Me.tbx_fecha = New System.Windows.Forms.TextBox()
+        Me.btn_ticket = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(723, 415)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(136, 175)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = CType(resources.GetObject("PictureBox2.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox2.Location = New System.Drawing.Point(390, 415)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(136, 175)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackgroundImage = CType(resources.GetObject("PictureBox3.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox3.Location = New System.Drawing.Point(555, 415)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(136, 175)
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackgroundImage = CType(resources.GetObject("PictureBox4.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox4.Location = New System.Drawing.Point(894, 415)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(136, 175)
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackgroundImage = CType(resources.GetObject("PictureBox5.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox5.Location = New System.Drawing.Point(1066, 415)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(136, 175)
+        Me.PictureBox5.TabIndex = 4
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.BackgroundImage = CType(resources.GetObject("PictureBox6.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox6.Location = New System.Drawing.Point(222, 415)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(136, 175)
+        Me.PictureBox6.TabIndex = 5
+        Me.PictureBox6.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.IndianRed
+        Me.Label1.Font = New System.Drawing.Font("HP Simplified", 22.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(457, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(468, 43)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "CINE LOS POLLOS HERMANOS"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.IndianRed
+        Me.Label2.Font = New System.Drawing.Font("HP Simplified", 22.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(602, 342)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(197, 43)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "CARTELERA"
+        '
+        'lbl_name
+        '
+        Me.lbl_name.AutoSize = True
+        Me.lbl_name.BackColor = System.Drawing.Color.White
+        Me.lbl_name.Font = New System.Drawing.Font("HP Simplified", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_name.Location = New System.Drawing.Point(100, 139)
+        Me.lbl_name.Name = "lbl_name"
+        Me.lbl_name.Size = New System.Drawing.Size(162, 21)
+        Me.lbl_name.TabIndex = 9
+        Me.lbl_name.Text = "NOMBRE Y APELLIDO:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.White
+        Me.Label3.Font = New System.Drawing.Font("HP Simplified", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(210, 215)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 21)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "SALA:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.White
+        Me.Label4.Font = New System.Drawing.Font("HP Simplified", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(184, 251)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(78, 21)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "ASIENTO:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.White
+        Me.Label5.Font = New System.Drawing.Font("HP Simplified", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(954, 177)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(145, 21)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "HORA DE FUNCIÓN:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.White
+        Me.Label6.Font = New System.Drawing.Font("HP Simplified", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(949, 218)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(150, 21)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "FECHA DE FUNCIÓN:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.White
+        Me.Label7.Font = New System.Drawing.Font("HP Simplified", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(908, 139)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(191, 21)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "NOMBRE DE LA PELICULA:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BackColor = System.Drawing.Color.White
+        Me.Label8.Font = New System.Drawing.Font("HP Simplified", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(134, 177)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(128, 21)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "IDENTIFICACIÓN:"
+        '
+        'tbx_name
+        '
+        Me.tbx_name.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbx_name.Location = New System.Drawing.Point(278, 136)
+        Me.tbx_name.Name = "tbx_name"
+        Me.tbx_name.Size = New System.Drawing.Size(147, 24)
+        Me.tbx_name.TabIndex = 16
+        '
+        'tbx_id
+        '
+        Me.tbx_id.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbx_id.Location = New System.Drawing.Point(278, 174)
+        Me.tbx_id.Name = "tbx_id"
+        Me.tbx_id.Size = New System.Drawing.Size(147, 24)
+        Me.tbx_id.TabIndex = 17
+        '
+        'tbx_stage
+        '
+        Me.tbx_stage.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbx_stage.Location = New System.Drawing.Point(278, 215)
+        Me.tbx_stage.Name = "tbx_stage"
+        Me.tbx_stage.Size = New System.Drawing.Size(147, 24)
+        Me.tbx_stage.TabIndex = 18
+        '
+        'tbx_site
+        '
+        Me.tbx_site.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbx_site.Location = New System.Drawing.Point(278, 251)
+        Me.tbx_site.Name = "tbx_site"
+        Me.tbx_site.Size = New System.Drawing.Size(147, 24)
+        Me.tbx_site.TabIndex = 19
+        '
+        'tbx_movie
+        '
+        Me.tbx_movie.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbx_movie.Location = New System.Drawing.Point(1105, 136)
+        Me.tbx_movie.Name = "tbx_movie"
+        Me.tbx_movie.Size = New System.Drawing.Size(146, 24)
+        Me.tbx_movie.TabIndex = 20
+        '
+        'tbx_hour
+        '
+        Me.tbx_hour.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbx_hour.Location = New System.Drawing.Point(1105, 176)
+        Me.tbx_hour.Name = "tbx_hour"
+        Me.tbx_hour.Size = New System.Drawing.Size(146, 24)
+        Me.tbx_hour.TabIndex = 21
+        '
+        'tbx_fecha
+        '
+        Me.tbx_fecha.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbx_fecha.Location = New System.Drawing.Point(1105, 218)
+        Me.tbx_fecha.Name = "tbx_fecha"
+        Me.tbx_fecha.Size = New System.Drawing.Size(146, 24)
+        Me.tbx_fecha.TabIndex = 22
+        '
+        'btn_ticket
+        '
+        Me.btn_ticket.Font = New System.Drawing.Font("HP Simplified", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_ticket.Location = New System.Drawing.Point(1094, 272)
+        Me.btn_ticket.Name = "btn_ticket"
+        Me.btn_ticket.Size = New System.Drawing.Size(209, 123)
+        Me.btn_ticket.TabIndex = 23
+        Me.btn_ticket.Text = "GENERAR TICKET"
+        Me.btn_ticket.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Yellow
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(1375, 640)
+        Me.Controls.Add(Me.btn_ticket)
+        Me.Controls.Add(Me.tbx_fecha)
+        Me.Controls.Add(Me.tbx_hour)
+        Me.Controls.Add(Me.tbx_movie)
+        Me.Controls.Add(Me.tbx_site)
+        Me.Controls.Add(Me.tbx_stage)
+        Me.Controls.Add(Me.tbx_id)
+        Me.Controls.Add(Me.tbx_name)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lbl_name)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.DoubleBuffered = True
+        Me.Name = "Form1"
+        Me.Text = "Ticket de Cine de los Pollos Hermanos"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lbl_name As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents tbx_name As TextBox
+    Friend WithEvents tbx_id As TextBox
+    Friend WithEvents tbx_stage As TextBox
+    Friend WithEvents tbx_site As TextBox
+    Friend WithEvents tbx_movie As TextBox
+    Friend WithEvents tbx_hour As TextBox
+    Friend WithEvents tbx_fecha As TextBox
+    Friend WithEvents btn_ticket As Button
+End Class
